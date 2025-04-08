02436bf96c03b5b43a0c9f00f2a0edd9dd7c6550
Seiva_Android_OPPO_Mike_2 <flowreshe.seiva.d.arve@gmail.com>
2025-04-08 17:14:25 +0100

    Pushed to github.com automatically (by ezGIT app)

--- garpho.sh
+++ garpho.sh
@@ -43,4 +43,34 @@
       unset v_list
     
 
+function f_filtrar_hashtags {
+
+
+# [fzf menu exemplo 1]
+   # Menu Simples
+
+   # Lista de opcoes para o menu `fzf`
+      Lz1='Save '; Lz2='<menu-terminal-command-here>'; Lz3="$Lz1\`$Lz2\`"; Lz4=$v_drya_fzf_menu_hist
+
+      L7='7. #forno ' 
+      L6='6. #batidos ' 
+      L5='5. #bolos' 
+      L4='4. #yammy-n' 
+      L3='3. #yammy-s' 
+      L1='1. Cancel'
+
+      L0="SELECT 1: Menu X: "
+      
+      v_list=$(echo -e "$L1 \n$L2 \n$L3 \n$L4 \n\n$Lz3" | fzf -m --cycle --prompt="$L0")
+
+   # Perceber qual foi a escolha da lista
+      [[ $v_list =~ $Lz3  ]] && echo "$Lz2" && history -s "$Lz2"
+      [[ $v_list =~ "2. " ]] && echo "uDev: $L2" && sleep 0.1 
+      [[ $v_list =~ "1. " ]] && echo "Canceled: $Lz2" && history -s "$Lz2"
+      unset v_list
+
+}
+    
+
+
 
