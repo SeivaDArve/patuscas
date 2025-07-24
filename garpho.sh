b81a0692a928e280daeccb0ade54ff646a17dc81
Seiva_Android_OPPO_Mike_2 <flowreshe.seiva.d.arve@gmail.com>
2025-07-24 21:24:27 +0100

    Upg: menu

--- garpho.sh
+++ garpho.sh
@@ -4,71 +4,28 @@
 
 
 
-function f_menu_2 {
-   # Menu Simples
-
-   # Lista de opcoes para o menu `fzf`
-      Lz1='Save '; Lz2='garpho'; Lz3="$Lz1\`$Lz2\`"; Lz4=$v_drya_fzf_menu_hist
-
-      L11='11. Help: Como escrever uma receita neste script'
-
-      L10='10. Go site   | Culinaria Ayurvedica (Aki Sinta Saude)'
-
-       L9='9.  Script    | multi cronometros | `D ca`'
-
-       L8='8.  Lista de compras | Criar nova '
-       L7='7.  Lista de compras | Ver e mover (mover para "adquiridos")'
-       L6='6.  Lista de compras | Ver "adquiridos"'
-
-       L5='5.  Marcar receitas (para ToDo list)' 
-
-       L4='4.  Ver:  Todos os ingredientes + produtos'
-       L3='3.  Ver:  Todas as receitas'
-       L2='2.  Ver:  Todos os Hashtag'
-
-       L1='1. Cancel'
-
-       L0="garpho: "
-      
-      v_list=$(echo -e "$L1 \n\n$L2 \n$L3 \n$L4 \n\n$L5 \n\n$L6 \n$L7 \n$L8 \n\n$L9 \n$L10 \n\n$L11 \n\n$Lz3" | fzf --cycle --prompt="$L0")
-
-   # Perceber qual foi a escolha da lista
-      [[ $v_list =~ $Lz3   ]] && echo "$Lz2" && history -s "$Lz2" 
-      [[ $v_list =~ "11. " ]] && echo "uDev: $L11"
-      [[ $v_list =~ "10. " ]] && xdg-open https://akisintasaude.pt 
-      [[ $v_list =~ "9.  " ]] && echo "uDev: $L9"
-      [[ $v_list =~ "8.  " ]] && v_items=$(less ${v_REPOS_CENTER}/garpho/all/ingredientes/all-ingredientes.txt | fzf -m) && [[ -n $v_items ]] && echo "$v_items" > ${v_REPOS_CENTER}/garpho/all/lista-de-compras/1-wish-list.txt
-      [[ $v_list =~ "7.  " ]] && echo "uDev: $L7"
-      [[ $v_list =~ "6.  " ]] && echo "uDev: $L6"
-      [[ $v_list =~ "5.  " ]] && echo "uDev: $L5"
-      [[ $v_list =~ "4.  " ]] && less ${v_REPOS_CENTER}/garpho/all/ingredientes/all-ingredientes.txt
-      [[ $v_list =~ "3.  " ]] && v_file=$(ls ${v_REPOS_CENTER}/garpho/all/receitas/texto/ | fzf) && less ${v_REPOS_CENTER}/garpho/all/receitas/texto/$v_file
-      [[ $v_list =~ "2.  " ]] && echo "uDev: $L2"
-      [[ $v_list =~ "1.  " ]] && echo "Canceled: $Lz2" && history -s "$Lz2"
-      unset v_list
-}
     
 
 function f_filtrar_hashtags {
 
    # Lista de opcoes para o menu `fzf`
-      Lz1='Save '; Lz2='<menu-terminal-command-here>'; Lz3="$Lz1\`$Lz2\`"; Lz4=$v_drya_fzf_menu_hist
+      Lz1='Save '; Lz2='ga hash'; Lz3="$Lz1\`$Lz2\`"; Lz4=$v_drya_fzf_menu_hist
 
-      L7='7. #forno ' 
-      L6='6. #batidos ' 
-      L5='5. #bolos' 
-      L4='4. #yammy-n' 
-      L3='3. #yammy-s' 
+      L6='6. #forno ' 
+      L5='5. #batidos ' 
+      L4='4. #bolos' 
+      L3='3. #yammy-n' 
+      L2='2. #yammy-s' 
       L1='1. Cancel'
 
-      L0="SELECT 1: Menu X: "
+      L0="garpho: Menu Hashtags: SELECT (1 ou +): "
       
-      v_list=$(echo -e "$L1 \n$L2 \n$L3 \n$L4 \n\n$Lz3" | fzf -m --cycle --prompt="$L0")
+      v_list=$(echo -e "$L1 \n$L2 \n$L3 \n$L4 \n$L5 \n$L6 \n\n$Lz3" | fzf -m --cycle --prompt="$L0")
 
    # Perceber qual foi a escolha da lista
-      [[ $v_list =~ $Lz3  ]] && echo "$Lz2" && history -s "$Lz2"
-      [[ $v_list =~ "2. " ]] && echo "uDev: $L2" && sleep 0.1 
-      [[ $v_list =~ "1. " ]] && echo "Canceled: $Lz2" && history -s "$Lz2"
+      [[ $v_list =~ $Lz3  ]] && echo "$Lz2" 
+      [[ $v_list =~ "2. " ]] && echo "uDev: $L2"
+      [[ $v_list =~ "1. " ]] && echo "Canceled: $Lz2"
       unset v_list
 }
     
@@ -78,43 +35,43 @@ function f_menu_lista_de_compras {
    # Gerir e criar listas de compras
       
    # Lista de opcoes para o menu `fzf`
-      Lz1='Save '; Lz2='<menu-terminal-command-here>'; Lz3="$Lz1\`$Lz2\`"; Lz4=$v_drya_fzf_menu_hist
+      Lz1='Save '; Lz2='ga menu-compras'; Lz3="$Lz1\`$Lz2\`"; Lz4=$v_drya_fzf_menu_hist
 
-       L9='9.  Script    | multi cronometros | `D ca`'
+      L11='11. Script | multi cronometros | `D ca`'
 
-       L4='4.  Lista de TODOS os ingredientes + Produtos conhecidos:'
+      L10='10. Agendar: Lista de Compras nova:'
+       L9='9.  Lista  | 1 | Nova Wish list      | Criar nova  (de 0 para 3)'
+       L8='8.  Lista  | 2 | Adicionar + Wish    | (de 0 para 3)'
+       L7='7.  Lista  | 4 | Catalogo Wish'
 
-       L4='4.  Gerir + Criar: Lista de Compras nova:'
-       L8='8.  Lista | 1 | Nova Wish list      | Criar nova  (de 0 para 3)'
-       L7='7.  Lista | 2 | Adicionar + Wish    | (de 0 para 3)'
-       L6='6.  Lista | 4 | Catalogo Wish'
+       L6='6.  Gerir + Criar: Lista do Carrinho de compras'
+       L5='5.  Lista | 3 | De: Wish para: Kart |  '
+       L4='4.  Lista | 4 | Catalogo Kart'
 
-       L4='4.  Gerir + Criar: Lista do Carrinho de compras'
-       L7='7.  Lista | 3 | De: Wish para: Kart |     ()'
-       L6='6.  Lista | 4 | Catalogo Kart'
-
-       L4='4.  Lista | 0 | Catalogo            | So ver: TODOS'
-       L4='4.  Ver  | TODOS os ingredientes + Produtos conhecidos'
+       L3='3.  Lista | 0 | Catalogo            | So ver: TODOS'
+       L2='2.  Ver   | TODOS os ingredientes + Produtos conhecidos'
        L1='1.  Cancel'
 
        L0="garpho: agendar compras: "
       
-      v_list=$(echo -e "$L1 \n$L2 \n$L3 \n$L4 \n\n$Lz3" | fzf --layout=reverse-list --pointer=">" --cycle --prompt="$L0")
-
-      #echo "comando" >> ~/.bash_history && history -n
-      #history -s "echo 'Olá, mundo!'"
+      v_list=$(echo -e "$L1 \n$L2 \n$L3 \n$L4 \n\n$Lz3" | fzf --pointer=">" --cycle --prompt="$L0")
 
    # Perceber qual foi a escolha da lista
-      [[ $v_list =~ $Lz3  ]] && echo "$Lz2" && history -s "$Lz2"
-      [[ $v_list =~ "4. " ]] && f_pin && echo "uDev: $L4"
-      [[ $v_list =~ "3. " ]] && echo "uDev: $L3" && history -s "$L3c" 
-      [[ $v_list =~ "2. " ]] && echo "uDev: $L2" && sleep 0.1 
-      [[ $v_list =~ "1. " ]] && echo "Canceled: $Lz2" && history -s "$Lz2"
+      [[ $v_list =~ $Lz3   ]] && echo "$Lz2" 
+      [[ $v_list =~ "11. " ]] && echo "uDev: $L11" 
+      [[ $v_list =~ "10. " ]] && echo "uDev: $L10" 
+      [[ $v_list =~ "9.  " ]] && echo "uDev: $L9" 
+      [[ $v_list =~ "8.  " ]] && echo "uDev: $L8" 
+      [[ $v_list =~ "7.  " ]] && echo "uDev: $L7" 
+      [[ $v_list =~ "6.  " ]] && echo "uDev: $L6" 
+      [[ $v_list =~ "5.  " ]] && echo "uDev: $L5" 
+      [[ $v_list =~ "4.  " ]] && echo "uDev: $L4" 
+      [[ $v_list =~ "3.  " ]] && echo "uDev: $L3" 
+      [[ $v_list =~ "2.  " ]] && echo "uDev: $L2"
+      [[ $v_list =~ "1.  " ]] && echo "Canceled: $Lz2"
       unset v_list
     
 }
-
-
 
 
 
@@ -125,44 +82,55 @@ function f_menu_principal {
       Lz1='Save '; Lz2='garpho'; Lz3="$Lz1\`$Lz2\`"; Lz4=$v_drya_fzf_menu_hist
 
       # Dolce Gusto Mimic Times (Esta em ca-lculadoras
-      #L11='11. Agricultura: quando plantar X planta
-      L11='11. Help | Como escrever uma receita neste script'
+      L10='10. Agricultura'    # quando plantar X planta
+       L9='9.  Help | boilerplate (Como escrever uma receita neste script)'
+       L8='8.  web  | Culinaria Ayurvedica (Aki Sinta Saude)'
+       L7='7.  Menu | multi cronometros | `D ca`' 
 
-      L10='10. Web  | Culinaria Ayurvedica (Aki Sinta Saude)'
+       L6='6.  Menu | Agendar/Gerir Compras'
+       L5='5.  Menu | Agendar/Gerir Receitas' 
 
-       L9='9.  Menu | multi cronometros | `D ca`' 
-
-       L6='6.  Menu | Agendar compras'
-       L5='5.  Menu | Agendar receitas' 
-
-       L4='4.  Ver  | TODOS os ingredientes + Produtos conhecidos'
+       L4='4.  Ver  | Todos os ingredientes + Produtos conhecidos'
        L3='3.  Ver  | Todas as receitas'
        L2='2.  Ver  | Todos os Hashtag'
-       L2="2.  Ver  $FZF_TOTAL_COUNT"
 
        L1='1. Cancel'
 
        L0="garpho: "
       
-      v_list=$(echo -e "$L1 \n\n$L2 \n$L3 \n$L4 \n\n$L5 \n\n$L6 \n$L7 \n$L8 \n\n$L9 \n$L10 \n\n$L11 \n\n$Lz3" | fzf --layout=reverse-list --reverse --cycle --prompt="$L0")
+      v_list=$(echo -e "$L1 \n\n$L2 \n$L3 \n$L4 \n\n$L5 \n$L6 \n\n$L7 \n$L8 \n$L9 \n$L10 \n\n$Lz3" | fzf --reverse --cycle --prompt="$L0")
 
    # Perceber qual foi a escolha da lista
-      [[ $v_list =~ $Lz3   ]] && echo "$Lz2" && history -s "$Lz2" 
+      [[ $v_list =~ $Lz3   ]] && echo "$Lz2" 
+      [[ $v_list =~ "12. " ]] && echo "uDev: $L12"
       [[ $v_list =~ "11. " ]] && echo "uDev: $L11"
       [[ $v_list =~ "10. " ]] && xdg-open https://akisintasaude.pt 
       [[ $v_list =~ "9.  " ]] && echo "uDev: $L9"
       [[ $v_list =~ "8.  " ]] && v_items=$(less ${v_REPOS_CENTER}/garpho/all/ingredientes/all-ingredientes.txt | fzf -m) && [[ -n $v_items ]] && echo "$v_items" > ${v_REPOS_CENTER}/garpho/all/lista-de-compras/1-wish-list.txt
       [[ $v_list =~ "7.  " ]] && echo "uDev: $L7"
-      [[ $v_list =~ "6.  " ]] && echo "uDev: $L6"
+      [[ $v_list =~ "6.  " ]] && f_menu_lista_de_compras
       [[ $v_list =~ "5.  " ]] && echo "uDev: $L5"
       [[ $v_list =~ "4.  " ]] && less ${v_REPOS_CENTER}/garpho/all/ingredientes/all-ingredientes.txt
       [[ $v_list =~ "3.  " ]] && v_file=$(ls ${v_REPOS_CENTER}/garpho/all/receitas/texto/ | fzf) && less ${v_REPOS_CENTER}/garpho/all/receitas/$v_file
-      [[ $v_list =~ "2.  " ]] && echo "uDev: $L2"
+      [[ $v_list =~ "2.  " ]] && f_filtrar_hashtags
       [[ $v_list =~ "1.  " ]] && echo "Canceled: $Lz2" && history -s "$Lz2"
       unset v_list
     
 }
 
-# uDev: Mesclar os 2 menus
+
+
+if [ -z $1 ]; then
+   # Se nao for apresentado nenhum argumento, apresentar o menu principal
    f_menu_principal
-   f_menu_2
+
+elif [ $1 == "menu-compras" ] || [ $1 == "c" ]; then
+   # Aprentar o menu de lista de compras diretamente
+   f_menu_lista_de_compras
+
+
+elif [ $1 == "hash" ]; then
+   # Aprentar o menu de hashtags diretamente
+   f_filtrar_hashtags
+
+fi
