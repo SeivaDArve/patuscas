94b1e27cb80dd6bde6ed569fe7bbc7a443294edf
Seiva_Android_A6_plus <flowreshe.seiva.d.arve@gmail.com>
2025-07-25 16:24:37 +0100

    Upg: `g r n` criar novas receitas com apoio de boilerplate

--- garpho.sh
+++ garpho.sh
@@ -2,6 +2,11 @@
 # Description: A script to cook
 
 
+# Sourcing DRYa Lib 2
+   v_lib2=${v_REPOS_CENTER}/DRYa/all/lib/drya-lib-2-tmp-n-config-files.sh
+   [[ -f $v_lib2 ]] && source $v_lib2 || read -s -n 1 -p "Error: drya-lib-2 does not exist"
+
+   # Example: f_create_tmp_file will create a temporary file stored at $v_tmp (with abs path, at ~/.tmp/...)
 
 
 function f_menu_receitas {
@@ -9,9 +14,9 @@ function f_menu_receitas {
    # Lista de opcoes para o menu `fzf`
       Lz1='Saved '; Lz2='ga receitas'; Lz3="$Lz1\`$Lz2\`"; Lz4=$v_drya_fzf_menu_hist
 
-      L9='9. Editar | boilerplate (para novas receita)'
-      L5='5. Criar nova Receita (com boilerplate)'
-      L4='4. Livros de Receitas'
+      L6='6. Editar | Boilerplate (para novas receita)'
+      L5='5. Criar  | Nova Receita (com boilerplate)'
+      L4='4. Abrir  | Livros de Receitas (em PDF)'
       L3='3. Agendar receitas'                                      
       L2='2. Ver todas as receitas'                                      
       L1='1. Cancel'
@@ -19,26 +24,68 @@ function f_menu_receitas {
       L0="SELECT 1: Menu X: "
       
    # Ordem de Saida das opcoes durante run-time
-      v_list=$(echo -e "$L1 \n$L2 \n$L3 \n$L4 \n$L5\n\n$Lz3" | fzf --no-info --pointer=">" --cycle --prompt="$L0")
+      v_list=$(echo -e "$L1 \n$L2 \n$L3 \n$L4 \n$L5 \n$L6 \n\n$Lz3" | fzf --no-info --pointer=">" --cycle --prompt="$L0")
 
    # Atualizar historico fzf automaticamente (deste menu)
       echo "$Lz2" >> $Lz4
    
    # Atuar de acordo com as instrucoes introduzidas pelo utilizador
       [[   $v_list =~ $Lz3  ]] && echo -e "Acede ao historico com \`D ..\` e encontra: \n > $Lz2"
-      [[   $v_list =~ "9. " ]] && vim ${v_REPOS_CENTER}/garpho/all/etc/boilerplate-receita-nova.txt
-      [[   $v_list =~ "5. " ]] && echo "uDev"
+      [[   $v_list =~ "6. " ]] && vim ${v_REPOS_CENTER}/garpho/all/etc/boilerplate-receita-nova.txt
+      [[   $v_list =~ "5. " ]] && f_criar_nova_receita_com_boilerplate
       [[   $v_list =~ "4. " ]] && v_livro=$(ls ${v_REPOS_CENTER}/garpho/all/receitas/pdf | fzf ) && echo "vai ser aberto: $v_livro" && xdg-open ${v_REPOS_CENTER}/garpho/all/receitas/pdf/$v_livro
       [[   $v_list =~ "3. " ]] && echo "uDev"
       [[   $v_list =~ "2. " ]] && v_file=$(ls ${v_REPOS_CENTER}/garpho/all/receitas/texto/ | fzf) && less ${v_REPOS_CENTER}/garpho/all/receitas/$v_file
       [[   $v_list =~ "1. " ]] && echo "Canceled" 
       unset v_list
 
-
-
-
 }
     
+function f_criar_nova_receita_com_boilerplate { 
+   # Usa drya-lib-2 e boilerplate-receita-nova.txt para criar uma nova receita do zero 
+
+   # Criar dois novos ficheiroz (com lib)
+      f_create_tmp_file 
+      v_tmp1=$v_tmp
+
+      sleep 1  # Serve para que ambos os ficheiros tenham nomes diferentes, porque o nome temporario é baseado na hora atual
+
+      f_create_tmp_file 
+      v_tmp2=$v_tmp
+         
+   # Caminho do boilerplate modelo
+      v_boi=${v_REPOS_CENTER}/garpho/all/etc/boilerplate-receita-nova.txt
+
+   # Caminho do diretorio onde vai ser guardado o ficheiro da receita
+      v_path=${v_REPOS_CENTER}/garpho/all/receitas/texto
+
+   # Decidir o nome da receita
+      echo    "Qual o nome que quer dar a receita? "
+      read -p " > " v_name
+      echo
+         
+      v_name_title="# Title: $v_name"
+
+   # Caminho final do ficheiro
+      v_perm=$v_path/$v_name
+
+   # Substituir o texto do ficheiro temporario pelo texto do boilerplate
+      cat $v_boi > $v_tmp1
+
+   # Substituir o titulo do documento (dentro do documento)
+      sed -i "/Title/d" $v_tmp1
+
+   # Criar "# Title" no segundo ficheiro temporario
+     echo "$v_name_title" > $v_tmp2
+
+   # Combinar ambos os ficheiros temporarios
+      cat $v_tmp1 >> $v_tmp2
+
+   # Concluir. Mover e renomear o ficheiro temporario
+      mv $v_tmp2 $v_perm
+      vim $v_perm
+}
+
 
 function f_filtrar_hashtags {
 
@@ -172,10 +219,15 @@ elif [ $1 == "menu-compras" ] || [ $1 == "c" ]; then
    f_menu_lista_de_compras
 
 
-elif [ $1 == "hash" ]; then
+elif [ $1 == "hash" ] || [ $1 == "H" ]; then
    # Aprentar o menu de hashtags diretamente
    f_filtrar_hashtags
 
-elif [ $1 == "receitas" ]; then
-   f_menu_receitas
+elif [ $1 == "receitas" ] || [ $1 == "r" ]; then
+   if [ -z $2 ]; then
+      f_menu_receitas
+
+   elif [ $2 == "nova" ] || [ $2 == "n" ]; then
+      f_criar_nova_receita_com_boilerplate 
+   fi
 fi
